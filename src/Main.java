03cf92b7d288d6306ee6887fe68359d62e75241f
shanuka-saranga <shanukasaranga974@gmail.com>
2026-04-04 14:06:48 +0530

    main menu

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        System.out.println("Main Menu");
+        System.out.println("1. Student Operations");
+        System.out.println("2. Marks Operations");
+        System.out.println("3. Exit");
+        System.out.println("Enter your choice: ");
+
+
+        choice = sc.nextInt();
+
+        switch (choice){
+            case 1:
+
+            case 2:
+
+
+            case 3:
+
+        }
+
+
+
+
+
 
 
 
